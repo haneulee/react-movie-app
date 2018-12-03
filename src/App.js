7aac6c330953328f95f10e1d4b71de9013d87308
haneulee <lovesky4294@gmail.com>
2018-12-03 14:05:57 +0900

    get movie data from api

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import Movie from "./Movie";
 import "./App.css";
 
 class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    this._getMovie();
+  }
+
+  _getMovie = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+    .then((response) => response.json())
+    .then((json) => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  _renderMovie = () => {
+    return this.state.movies.map((data, index) => <Movie key={index} title={data.title} poster={data.large_cover_image}></Movie>);
+  }
+
   render() {
     return (
       <div className="App">
-        <Movie />
+        {this.state.movies ? this._renderMovie() : "Loading..."}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
+import propTypes from "prop-types";
 import "./Movie.css";
 
 class Movie extends Component {
+  static propTypes = {
+    title: propTypes.string.isRequired,
+    poster: propTypes.string.isRequired
+  }
+
   render() {
+    const {title, poster} = this.props;
     return (
       <div>
-        <h1>hello, this is movie app</h1>
+        <h1>
+          {title}
+        </h1>
+        <MoviePoster poster={poster}></MoviePoster>
       </div>
     );
   }
+}
+
+function MoviePoster({poster}) {
+  return (
+    <img src={poster} alt=""/>
+  )
+}
+
+MoviePoster.propTypes = {
+  poster: propTypes.string.isRequired
 }
 
 export default Movie;
